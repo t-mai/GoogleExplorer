369358285b107a5cb2b039296ffe7982345e986f
Trang <t-mai@amida.co.jp>
2016-06-30 16:47:54 +0900

    Fix some bug of getting data

--- example.py
+++ example.py
@@ -1,12 +1,13 @@
-from xgoogle.search import GoogleSearch, SearchError
+from xgoogle.scraper import GoogleScraper, ScraperError
 try:
-  gs = GoogleSearch("quick and dirty", random_agent=True, debug=True)
-  gs.results_per_page = 50
+  gs = GoogleScraper("gion matsuri", random_agent=True, debug=True)
+  gs.results_per_page = 10
   results = gs.get_results()
   for res in results:
-    print res.title.encode("utf8")
-    print res.desc.encode("utf8")
-    print res.url.encode("utf8")
+    print "Result title: %s" % res.title.encode("utf8")
+    print "Result description: %ss" % res.desc.encode("utf8")
+    print "Result url: %s" % res.url.encode("utf8")
+    print "Content of url: %s" % res.content.encode("utf8")
     print
-except SearchError, e:
+except ScraperError, e:
   print "Search failed: %s" % e
